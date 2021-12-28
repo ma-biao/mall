e7118398b7a25e4513ef470feaa7ecc3e5716042
LAPTOP-JDE5A8QP\马彪 <626811344@qq.com>
2021-12-28 10:48:56 +0800

    OpenFeign远程调用

--- mall-member/src/main/java/com/mabiao/mall/member/MallMemberApplication.java
+++ mall-member/src/main/java/com/mabiao/mall/member/MallMemberApplication.java
@@ -3,7 +3,18 @@ package com.mabiao.mall.member;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ * 想要远程调用别的服务：
+ * 1. 引入open-feign
+ * 2. 编写一个接口，告诉SpringCloud这个接口需要调用远程服务
+ *      声明接口的每一个方法都是调用哪个远程服务的哪个请求
+ * 3. 开启远程调用功能@EnableFeignClients(basePackages = "com.mabiao.mall.member.feign")
+ *      自动扫描basePackages下所有标注@FeignClient的接口
+ */
+
+@EnableFeignClients(basePackages = "com.mabiao.mall.member.feign")
 @EnableDiscoveryClient
 @SpringBootApplication
 public class MallMemberApplication {
---
+++ mall-member/src/main/java/com/mabiao/mall/member/feign/CouponFeignService.java
@@ -0,0 +1,14 @@
+package com.mabiao.mall.member.feign;
+
+import com.mabiao.common.utils.R;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 声明式的远程调用
+ */
+@FeignClient("mall-coupon")
+public interface CouponFeignService {
+    @RequestMapping("/coupon/coupon/member/list")
+    public R memberCoupons();
+}
