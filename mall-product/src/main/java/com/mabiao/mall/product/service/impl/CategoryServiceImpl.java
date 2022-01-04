f693f7d9d181f52b83174436d4c52d58b95d355d
LAPTOP-JDE5A8QP\马彪 <626811344@qq.com>
2022-01-04 11:18:58 +0800

    递归树形结构数据获取

--- mall-product/src/main/java/com/mabiao/mall/product/service/impl/CategoryServiceImpl.java
+++ mall-product/src/main/java/com/mabiao/mall/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,12 @@
 package com.mabiao.mall.product.service.impl;
 
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -24,6 +29,41 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        // 1. 查出所有分类
+        List<CategoryEntity> categoryEntities = baseMapper.selectList(null);
+
+        // 2. 组装成父子的树形结构
+        // 2.1 找到所有的一级分类
+        List<CategoryEntity> level1Menus = categoryEntities.stream().filter(categoryEntity ->
+                categoryEntity.getParentCid() == 0
+        ).map((menu) -> {
+            menu.setChildren(getChildrens(menu, categoryEntities));
+            return menu;
+        }).sorted((menu1, menu2) -> {
+            return (menu1.getSort() == null ? 0 : menu1.getSort()) - (menu2.getSort() == null ? 0 : menu2.getSort());
+        }).collect(Collectors.toList());
+
+        return level1Menus;
+    }
+
+    // 递归查找所有菜单的子菜单
+    private List<CategoryEntity> getChildrens(CategoryEntity root, List<CategoryEntity> all){
+        List<CategoryEntity> children = all.stream().filter(categoryEntity ->
+                categoryEntity.getParentCid() == root.getCatId()
+        ).map(categoryEntity -> {
+            // 找到子菜单
+            categoryEntity.setChildren(getChildrens(categoryEntity, all));
+            return categoryEntity;
+        }).sorted((menu1, menu2) -> {
+            // 菜单的排序
+            return (menu1.getSort() == null ? 0 : menu1.getSort()) - (menu2.getSort() == null ? 0 : menu2.getSort());
+        }).collect(Collectors.toList());
+
+        return children;
     }
 
 }
