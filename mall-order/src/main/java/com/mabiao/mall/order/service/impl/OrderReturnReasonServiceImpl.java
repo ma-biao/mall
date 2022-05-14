8740888564ce5276d0227e06ebea642b56f8419f
mabiao <626811344@qq.com>
2022-05-14 17:19:07 +0800

    [订单服务1.5: 补充部分未提交]

---
+++ mall-order/src/main/java/com/mabiao/mall/order/service/impl/OrderReturnReasonServiceImpl.java
@@ -0,0 +1,29 @@
+package com.mabiao.mall.order.service.impl;
+
+import org.springframework.stereotype.Service;
+import java.util.Map;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.mabiao.common.utils.PageUtils;
+import com.mabiao.common.utils.Query;
+
+import com.mabiao.mall.order.dao.OrderReturnReasonDao;
+import com.mabiao.mall.order.entity.OrderReturnReasonEntity;
+import com.mabiao.mall.order.service.OrderReturnReasonService;
+
+
+@Service("orderReturnReasonService")
+public class OrderReturnReasonServiceImpl extends ServiceImpl<OrderReturnReasonDao, OrderReturnReasonEntity> implements OrderReturnReasonService {
+
+    @Override
+    public PageUtils queryPage(Map<String, Object> params) {
+        IPage<OrderReturnReasonEntity> page = this.page(
+                new Query<OrderReturnReasonEntity>().getPage(params),
+                new QueryWrapper<OrderReturnReasonEntity>()
+        );
+
+        return new PageUtils(page);
+    }
+
+}
